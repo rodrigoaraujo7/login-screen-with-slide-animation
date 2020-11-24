0713f0197c709411db8190963c5ce22cffc0c167
Rodrigo Leite Araujo <rodrigoleitearaujo777@gmail.com>
2020-11-24 14:38:34 -0300

    slide animation

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,17 +1,45 @@
 function slide1() {
     const img = document.querySelector('#image')
     img.src = './components/images/image1.png'
-    setTimeout("slide2()", 2500)
+
+    setTimeout("slideIn()", 0)
+    setTimeout("slideOut()", 2500)
+
+    setTimeout("slide2()", 3000)
 }
 
 function slide2() {
     const img = document.querySelector('#image')
     img.src = './components/images/image2.png'
-    setTimeout("slide3()", 2500)
+
+    setTimeout("slideIn()", 0)
+    setTimeout("slideOut()", 2500)
+
+    setTimeout("slide3()", 3000)
 }
 
 function slide3() {
     const img = document.querySelector('#image')
     img.src = './components/images/image3.png'
-    setTimeout("slide1()", 2500)
+
+    setTimeout("slideIn()", 0)
+    setTimeout("slideOut()", 2500)
+
+    setTimeout("slide1()", 3000)
+}
+
+function slideIn() {
+    document.getElementById('image').classList.add("animate-in")
+
+    setTimeout(() => {
+        document.getElementById('image').classList.remove("animate-in")
+    }, 550)
+}
+
+function slideOut() {
+    document.getElementById('image').classList.add("animate-out")
+
+    setTimeout(() => {
+        document.getElementById('image').classList.remove("animate-out")
+    }, 550)
 }
